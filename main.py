49e7937096576ed300089fac619497c4def7a869
aluchieneseiv <52246019+aluchieneseiv@users.noreply.github.com>
2019-09-25 17:44:34 +0300

    add VSCode to gitignore

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 
-class CalcApp(App):
+class PyCalcApp(App):
     def build(self):
         return FloatLayout()
 
 if __name__ == '__main__':
-    CalcApp().run()
+    PyCalcApp().run()
