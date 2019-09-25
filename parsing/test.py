0ca38a57d408a21651eab377b64b762f83672343
decadedelta <decadedelta@gmail.com>
2019-09-25 21:36:52 +0300

    add 2 commands

--- parsing/test.py
+++ parsing/test.py
@@ -10,29 +10,51 @@ class CalculateTree(Transformer):
     number = float
     pow = lambda _, a, b: a ** b
 
+    builtins = {
+        "sin": math.sin,
+        "cos": math.cos,
+        "tan": math.tan,
+        "cot": lambda x: 1 / math.tan(x),
+        "arcsin": math.asin,
+        "arccos": math.acos,
+        "atan": math.atan,
+        "atan2": math.atan2,
+        "acot": lambda x: math.atan(1 / x),
+        "version": "0.0.1"
+    }
+
     def evaluate(self, fname, *args):
-        return self.vars[fname](*args)
+        return self.var(fname)(*args)
 
     def __init__(self):
-        self.vars = {
-            "sin": math.sin,
-            "cos": math.cos,
-            "tan": math.tan,
-            "cot": lambda x: 1 / math.tan(x),
-            "arcsin": math.asin,
-            "arccos": math.acos,
-            "atan": math.atan,
-            "atan2": math.atan2,
-            "acot": lambda x: math.atan(1 / x),
-            "version" : "0.0.1"
-        }
+        self.vars = {}
 
     def assign_var(self, name, value):
         self.vars[name] = value
         return value
 
     def var(self, name):
-        return self.vars[name]
+        if name in self.vars:
+            return self.vars[name]
+        
+        return self.builtins[name]
+
+    def clear_vars(self, *args):
+        if not args:
+            self.vars.clear()
+            return "All variables cleared"
+        else:
+            for name in args:
+                del self.vars[name]
+
+            v = ", ".join(args)
+            
+            return f"Variables {v} cleared"
+
+    def show_vars(self):
+        s = "\n".join(map(lambda i: f"  {i[0]} = {i[1]}", self.vars.items()))
+
+        return s
 
 
 calc_parser = Lark(grammar, parser='lalr', transformer=CalculateTree())
