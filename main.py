c959ec1a26e5cea4dee63908239989347bdb84ae
Kimerth <motfoleas@yahoo.com>
2019-10-03 20:48:28 +0300

    getting there ui

--- main.py
+++ main.py
@@ -1,94 +1,66 @@
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.properties import ObjectProperty
-from kivy.properties import StringProperty
+from kivy.clock import Clock
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.textinput import TextInput
+
 from newlang import State
 
-Builder.load_string('''
-<AppView>:
-    orientation: 'vertical'
-    ActionBar:
-        pos_hint: {'top':1}
-        ActionView:
-            use_separator: True
-            ActionPrevious:
-                title: 'CalcPy'
-                with_previous: False
-            ActionOverflow:
-            ActionButton:
-                important: True
-                text: 'Clear'
-            ActionGroup:
-                ActionButton:
-                    text: 'Save'
-                ActionButton:
-                    text: 'Load'
-    ScrollableText:
-
-<ScrollableText>:
-    BoxLayout:
-        size_hint_y: None
-        orientation: 'vertical'
-        height: self.minimum_height
-
-        TextLine:
-        TextLine:
-        TextLine:
-        TextLine:
-        TextLine:
-        TextLine:
-        TextLine:
-        TextLine:
-        TextLine:
-        TextLine:
-        
-<TextLine>:
-    input: "test input"
-    output: "test output"
-    focused: input.focused
-    size_hint_y: None
-    orientation: 'vertical'
-    height: self.minimum_height
-    TextInput:
-        id: input
-        size_hint_y: None
-        multiline: False
-        height: self.font_size * 1.25 + 10
-        text: root.input
-        foreground_color: (1, 1, 1, 1)
-        cursor_color: (0.8, 0.8, 0.8, 0.9)
-        background_color: (0.2, 0.2, 0.2, 1) if self.focus else (0.1, 0.1, 0.1, 1)
-    TextOutput:
-        output: "oof test\\n" * 3
-
-<TextOutput>:
-    size_hint_y: None
-    id: label
-    height: self.texture_size[1]
-    text_size: self.width, None
-    text: self.output
-    padding: 5, 5
-''')
+Builder.load_file('./main.kv')
 
 state = State()
 
-class TextLine(BoxLayout):
-    input = StringProperty()
-    output = StringProperty()
+class TextLine(RelativeLayout):
+
+    def code_interpret(self):
+        codeinput = self.children[1]
+        codeoutput = self.children[0]
+
+        res, err = state.parse(codeinput.text)
+
+        if err:
+            codeoutput.text = str(err)
+            codeoutput.color = (1, 0, 0, 1)
+        else:
+            codeoutput.text = str(res)
+
 
 class ScrollableText(ScrollView):
-    pass
+    def __init__(self,  **kwargs):
+        super(ScrollableText, self).__init__(**kwargs)
+        self.layout = BoxLayout(orientation='vertical', spacing=10, size_hint_y=None)
+        self.layout.bind(minimum_height=self.layout.setter('height'))
+
+        self.add_new_textline()
+
+        self.add_widget(self.layout)
+
+    def add_new_textline(self):
+        textline = TextLine()
+
+        codeinput = textline.children[1]
+
+        codeinput.bind(on_text_validate=lambda _: self.add_new_textline())
+        codeinput.bind(on_text_validate=lambda _: textline.code_interpret())
+        codeinput.focus = True
+        
+        self.layout.add_widget(textline)
 
 class AppView(BoxLayout):
     pass
 
-class TextOutput(Label):
-    output = StringProperty()
+class CodeOutput(Label):
+    pass
+
+class CodeInput(TextInput):
+    pass
 
 class CalcPyApp(App):
     def build(self):
